824d2b8286eb6a9833818607d6b3a78f357dc402
heyiamkrishna <techembersofficial@email.com>
2024-09-15 12:32:02 +0530

    make button componennt

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { FaArrowTurnUp } from "react-icons/fa6";
+function Button() {
+  return (
+    <div className='px-8 py-2 bg-zinc-100 flex  text-zinc-800 rounded-full'>
+    <div className="get flex  capitalize ">get started</div>
+   <h3>lorem</h3>
+    </div>
+  )
+}
+
+export default Button
